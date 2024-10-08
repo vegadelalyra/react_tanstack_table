6e1cf537606e708ffdedd26c92be0f303511aaab
Vegadelalyra <lyradeperseo@gmail.com>
2024-10-08 15:01:16 -0500

    feat: react tanstack table with frankenstein

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SimpleTable from './components/SimpleTable';
 import data from './MOCK_DATA.json';
+import otherData from './countries.json';
 import dayjs from 'dayjs';
+import UsersTable from './components/UsersTable';
 
 const App = () => {
   const columns = [
@@ -10,10 +13,6 @@ const App = () => {
       accessorKey: 'id',
       footer: 'Mi ID',
     },
-    // {
-    //   header: 'Nombres y Apellidos',
-    //   accessorFn: row => `${row.name} ${row.lastname}`,
-    // },
     {
       header: 'Nombres Completos',
       columns: [
@@ -29,16 +28,6 @@ const App = () => {
         },
       ],
     },
-    // {
-    //   header: 'Nombres',
-    //   accessorKey: 'name',
-    //   footer: 'Mi nombre',
-    // },
-    // {
-    //   header: 'Apellidos',
-    //   accessorKey: 'lastname',
-    //   footer: 'Mi apellido',
-    // },
     {
       header: 'Email',
       accessorKey: 'email',
@@ -57,10 +46,62 @@ const App = () => {
     },
   ];
 
+  const otherColumns = [
+    {
+      header: 'ID',
+      accessorKey: 'id',
+      footer: 'ID',
+    },
+    {
+      header: 'País',
+      accessorKey: 'country',
+      footer: 'País',
+    },
+    {
+      header: 'Código de país',
+      accessorKey: 'countrycode',
+      footer: 'Código de país',
+    },
+    {
+      header: 'Correo Electrónico',
+      accessorKey: 'email',
+      footer: 'Correo Electrónico',
+    },
+  ];
+
   return (
-    <div>
-      <SimpleTable data={data} columns={columns} />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Main Table</Link>
+            </li>
+            <li>
+              <Link to='/other-table'>Other Table</Link>
+            </li>
+            <li>
+              <Link to='/users-table'>Users Table</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route
+            path='/'
+            element={<SimpleTable data={data} columns={columns} />}
+          />
+          <Route
+            path='/other-table'
+            element={<SimpleTable data={otherData} columns={otherColumns} />}
+          />
+          <Route
+            path='/users-table'
+            element={<UsersTable data={otherData} columns={otherColumns} />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/components/UsersTable.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { FormEvent } from 'react';
+import {
+  flexRender,
+  useReactTable,
+  getCoreRowModel,
+  createColumnHelper,
+  SortingState,
+} from '@tanstack/react-table';
+
+export type User = {
+  id: number;
+  name: string;
+  age: number;
+};
+
+const columnHelper = createColumnHelper<User>();
+
+const columns = [
+  columnHelper.accessor('id', {
+    header: () => 'ID',
+    cell: info => info.getValue(),
+  }),
+  columnHelper.accessor('name', {
+    header: () => 'Name',
+    cell: info => info.getValue(),
+  }),
+  columnHelper.accessor('age', {
+    header: () => 'Age',
+    cell: info => info.getValue(),
+  }),
+];
+
+const UsersTable = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [users, setUsers] = useState([]);
+  const [inputSearchValue, setInputSearchValue] = useState('');
+  const [sorting, setSorting] = useState<SortingState>([]);
+
+  const submitSearchForm = (e: FormEvent) => {
+    e.preventDefault();
+    setSearchValue(inputSearchValue);
+  };
+
+  const table = useReactTable({
+    data: users,
+    columns,
+    debugTable: true,
+    getCoreRowModel: getCoreRowModel(),
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+  });
+
+  useEffect(() => {
+    console.log('sorting', sorting)
+    const order = sorting[0]?.desc ? 'desc' : 'asc'
+    const sort = sorting[0]?.id ?? 'id'
+    const url = `http://localhost:3004/users?name_like=${searchValue}&_sort=${sort}&_order=${order}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(users => setUsers(users));
+  }, [searchValue, sorting]);
+
+  return (
+    <div>
+      <div className='search-bar'>
+        <form onSubmit={submitSearchForm}>
+          <input
+            type='text'
+            placeholder='Search...'
+            value={inputSearchValue}
+            onChange={e => setInputSearchValue(e.target.value)}
+          />
+        </form>
+      </div>
+      <table className='users-table'>
+        <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id} className='users-table-cell'>
+                  <div
+                    {...{
+                      className: header.column.getCanSort()
+                        ? 'cursor-pointer select-none'
+                        : '',
+                      onClick: header.column.getToggleSortingHandler(),
+                    }}>
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                    {
+                      {
+                        asc: 'UP',
+                        desc: 'DO',
+                      }[(header.column.getIsSorted() as string) ?? null]
+                    }
+                  </div>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id} className='users-table-cell'>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UsersTable;
